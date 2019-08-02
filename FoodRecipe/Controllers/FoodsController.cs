832eb21c2f6dd151accbeff98f7bd49310b0eb76
jkim647 <41564494+jkim647@users.noreply.github.com>
2019-08-02 16:19:05 -0700

    first stage

---
+++ FoodRecipe/Controllers/FoodsController.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using FoodRecipe.Model;
+
+namespace FoodRecipe.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class FoodsController : ControllerBase
+    {
+        private readonly FoodContext _context;
+
+        public FoodsController(FoodContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/Foods
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Food>>> GetFood()
+        {
+            return await _context.Food.ToListAsync();
+        }
+
+        // GET: api/Foods/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Food>> GetFood(int id)
+        {
+            var food = await _context.Food.FindAsync(id);
+
+            if (food == null)
+            {
+                return NotFound();
+            }
+
+            return food;
+        }
+
+        // PUT: api/Foods/5
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutFood(int id, Food food)
+        {
+            if (id != food.FoodId)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(food).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!FoodExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/Foods
+        [HttpPost]
+        public async Task<ActionResult<Food>> PostFood(Food food)
+        {
+            _context.Food.Add(food);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction("GetFood", new { id = food.FoodId }, food);
+        }
+
+        // DELETE: api/Foods/5
+        [HttpDelete("{id}")]
+        public async Task<ActionResult<Food>> DeleteFood(int id)
+        {
+            var food = await _context.Food.FindAsync(id);
+            if (food == null)
+            {
+                return NotFound();
+            }
+
+            _context.Food.Remove(food);
+            await _context.SaveChangesAsync();
+
+            return food;
+        }
+
+        private bool FoodExists(int id)
+        {
+            return _context.Food.Any(e => e.FoodId == id);
+        }
+    }
+}
---
+++ FoodRecipe/Model/Food.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace FoodRecipe.Model
+{
+    public partial class Food
+    {
+        public Food()
+        {
+            Restaurant = new HashSet<Restaurant>();
+        }
+
+        public int FoodId { get; set; }
+        [Required]
+        [StringLength(500)]
+        public string Title { get; set; }
+        public int? Cal { get; set; }
+        [StringLength(500)]
+        public string Ingredients { get; set; }
+        public int? Width { get; set; }
+        public int? Height { get; set; }
+        [Column("isFavourite")]
+        public bool? IsFavourite { get; set; }
+
+        [InverseProperty("Food")]
+        public virtual ICollection<Restaurant> Restaurant { get; set; }
+    }
+}
